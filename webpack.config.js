819f2201152ba69809892a76e82f5849d5ecb2e3
mara <mspx2023@gmail.com>
2023-06-09 15:31:26 +0200

    Check ingnoring

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,14 @@ const path = require('path');
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
-        
+      hash: true,
+      title: 'Webpack Example App',
+      header: 'Webpack Example Title',
+      metaDesc: 'Webpack Example Description',
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
     })
-  ]
+  ],
+  
 };
